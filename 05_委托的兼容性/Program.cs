c0bc7bff5574d760aff8106d62fa2d85b3694c36
俊华 蔡 <1032031912@qq.com>
2020-08-26 08:56:32 +0800

    添加委托准备绑定的方法

--- 05_委托的兼容性/Program.cs
+++ 05_委托的兼容性/Program.cs
@@ -16,6 +16,16 @@ namespace _05_委托的兼容性
         public delegate void HandleModthod1(Manmal m);
         public delegate void handleModthod2(Dog d);
 
+        //委托绑定对应的方法
+        public static Manmal FirstHandler()
+        { Console.WriteLine("First Handler");return null; }
+
+        public static Dog SecondHandler()
+        { Console.WriteLine("Second Handler");return null; }
+
+        public static void ThirdHandler(Manmal m)
+        { Console.WriteLine("Third Handler"); }
+
         static void Main(string[] args)
         {
         }
