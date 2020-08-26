60dbaa8b24d7ba839ec1700a19db582f3b972c9d
俊华 蔡 <1032031912@qq.com>
2020-08-26 08:51:38 +0800

    创建父类与子类，定义不同的委托

---
+++ 05_委托的兼容性/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05_委托的兼容性
+{
+    //创建了两个类，一个父类一个派生的子类
+    class Manmal { }
+    class Dog : Manmal { }
+
+    class Program
+    {
+        //定义不同的委托
+        public delegate Manmal HandlerMethod();
+        public delegate void HandleModthod1(Manmal m);
+        public delegate void handleModthod2(Dog d);
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
