0e00b9041008e45b0087ce7f27c31996b9694689
俊华 蔡 <1032031912@qq.com>
2020-08-27 10:41:54 +0800

    添加标准事件模型

---
+++ 08_标准事件模型/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _08_标准事件模型
+{
+    //步骤1:声明提供事件数据的类
+    public class NameListEventArgs:EventArgs
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        //构造函数
+        public NameListEventArgs(string name,int count)
+        {
+            this.Name = name;
+            this.Count = count;
+        }
+    }
+    //步骤2:声明事件处理委托
+    public delegate void NameListEventHandler(object source, NameListEventArgs args);
+    //步骤3:声明应发事件的类 (事件生产类) 妈妈类
+    public class NameList
+    {
+        ArrayList list;
+        //步骤4：在事件生产类中,声明事件  (妈妈类的事件)
+        public event NameListEventHandler nameListEvent;
+        
+        public NameList()
+        {
+            list = new ArrayList();
+        }
+        //添加使用的功能函数，(等于:妈妈做好饭)
+        public void Add(string Name)
+        {
+            list.Add(Name);
+            //步骤5:在事件生成类中,实现产生事件的代码
+            if (nameListEvent !=null )
+            {
+                nameListEvent(this, new NameListEventArgs(Name, list.Count));
+            }
+        }
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
